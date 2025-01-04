eebe1cc2d59d29b999031ca1b2d82aaf03eafa51
rashedevs <rasheduap2015@gmail.com>
2025-01-05 05:02:57 +0600

    explored tuple type on ts

---
+++ user_defined_type/tuple-type.js
@@ -0,0 +1,5 @@
+//mixed data for key/value pairs
+var user;
+user = [101, "Rashed"];
+user.push(102, "Zaman");
+console.log(user);
---
+++ user_defined_type/tuple-type.ts
@@ -0,0 +1,8 @@
+//mixed data for key/value pairs
+
+let user:[number,string]
+user=[101,"Rashed"]
+
+user.push(102,"Zaman");
+
+console.log(user);
