9ef28d97b0615267e9df57a1e446d2664c5c76e5
rashedevs <rasheduap2015@gmail.com>
2025-01-10 05:14:36 +0600

    explored object with array and methods

--- user_defined_type/object-type.js
+++ user_defined_type/object-type.js
@@ -1,2 +1,17 @@
-var user1 = { name: "John", userID: 101, passed: true };
-console.log('user1', user1);
+// let users : object;
+// users = {namespace: 'users', username:'kkk'}
+// let users : object[];
+// users = [{namespace: 'users', username:'kkk'}]
+var users;
+users = [];
+var user1;
+// let user1 : {name: string, userID?: number, passed: boolean} //optional userID
+user1 = { name: "John", userID: 101, passed: true };
+users.push(user1);
+var user2;
+user2 = { name: "Wick", userID: 102, passed: false };
+users.push(user2);
+// console.log('users',  users);
+for (var key in users) {
+    console.log(users[key]["name"]);
+}
--- user_defined_type/object-type.ts
+++ user_defined_type/object-type.ts
@@ -1,2 +1,23 @@
-let user1 = {name: "John", userID: 101, passed: true}
-console.log('user1', user1);
+// let users : object;
+// users = {namespace: 'users', username:'kkk'}
+
+
+// let users : object[];
+// users = [{namespace: 'users', username:'kkk'}]
+
+let users : object[];
+users=[];
+
+let user1 : {name: string, userID: number, passed: boolean}
+// let user1 : {name: string, userID?: number, passed: boolean} //optional userID
+user1 = {name: "John", userID: 101, passed: true}
+users.push(user1);
+
+let user2 : {name: string, userID: number, passed: boolean}
+user2 = {name: "Wick", userID: 102, passed: false}
+users.push(user2);
+
+// console.log('users',  users);
+for (const key in users) {
+    console.log(users[key]["name"]);
+}
