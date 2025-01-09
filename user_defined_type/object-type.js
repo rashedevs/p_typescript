0dfeb6320ccf353d7310ffe635d1b4515f317a7c
rashedevs <rasheduap2015@gmail.com>
2025-01-10 04:50:22 +0600

    explored object data type for ts

---
+++ user_defined_type/object-type.js
@@ -0,0 +1,2 @@
+var user1 = { name: "John", userID: 101, passed: true };
+console.log('user1', user1);
---
+++ user_defined_type/object-type.ts
@@ -0,0 +1,2 @@
+let user1 = {name: "John", userID: 101, passed: true}
+console.log('user1', user1);
